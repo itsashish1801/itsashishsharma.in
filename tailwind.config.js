00ca3e3954b761572b0255beaaf206924735d001
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-01-27 15:39:43 +0530

    Add color pallete.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,50 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: 'var(--color-white)',
+      black: 'var(--color-black)',
+
+      gray: {
+        100: 'var(--color-gray-100)',
+        200: 'var(--color-gray-200)',
+        300: 'var(--color-gray-300)',
+        400: 'var(--color-gray-400)',
+        500: 'var(--color-gray-500)',
+        600: 'var(--color-gray-600)',
+        700: 'var(--color-gray-700)',
+        800: 'var(--color-gray-800)',
+        900: 'var(--color-gray-900)',
+      },
+      slate: {
+        500: 'var(--color-slate-500)',
+      },
+      team: {
+        unknown: 'var(--color-team-unknown)',
+        current: 'var(--color-team-current)',
+        yellow: 'var(--color-team-yellow)',
+        blue: 'var(--color-team-blue)',
+        red: 'var(--color-team-red)',
+      },
+      yellow: {
+        500: 'var(--color-yellow-500)',
+        '500-inverted': 'var(--color-yellow-500-inverted)',
+      },
+      blue: {
+        100: 'var(--color-blue-100)',
+        500: 'var(--color-blue-500)',
+      },
+      red: {
+        500: 'var(--color-red-500)',
+      },
+      green: {
+        100: 'var(--color-green-100)',
+        500: 'var(--color-green-500)',
+        600: 'var(--color-green-600)',
+      },
+    },
     extend: {},
   },
   plugins: [],
