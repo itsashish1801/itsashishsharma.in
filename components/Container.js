2833e886dc9fc7a849bfe3f7a456daf2ee3a5ca0
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-01-29 16:30:22 +0530

    Fix max-width.

--- components/Container.js
+++ components/Container.js
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 function Container({ className, ...props }) {
   return (
-    <div className={clsx('mx-auto max-w-3xl px-4', className)} {...props} />
+    <div className={clsx('mx-auto max-w-2xl px-4', className)} {...props} />
   );
 }
 
