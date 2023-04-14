712259a9c946330f240b803ef451e49fdc9aebc9
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-04-14 13:22:06 +0530

    Build Footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,15 @@
+import Logo from './Logo';
+
 function Footer() {
-  return <div></div>;
+  return (
+    <footer className='py-16'>
+      <Logo />
+      <p className='mt-4 font-medium'>Thanks for reading!</p>
+      <p className='mt-1 text-sm text-gray-500 dark:text-slate-500'>
+        All rights reserved &copy; Ashish Sharma 2023
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
