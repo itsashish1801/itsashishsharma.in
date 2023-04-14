86af0d36014cc7e628e35ac63efddcaf3a2e78fb
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-04-14 12:21:13 +0530

    Add functions to get Blogs data.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Head from 'next/head';
 
 interface LayoutProps {
   children: ReactElement;
@@ -10,11 +11,21 @@ interface LayoutProps {
 
 function Layout({ children, className }: LayoutProps) {
   return (
-    <div className={`container max-w-3xl mx-auto px-5 lg:px-0 ${className}`}>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Ashish Sharma</title>
+        <meta
+          name='description'
+          content="Hi, I'm Ashish Sharma. This is my personal blog where I share my thoughts and learnings."
+        />
+        <link rel='icon' href='/favicon.svg' />
+      </Head>
+      <div className={`container max-w-3xl mx-auto px-5 lg:px-0 ${className}`}>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ lib/posts.ts
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { bundleMDX } from 'mdx-bundler';
+import remarkGfm from 'remark-gfm';
+
+type Frontmatter = {
+  title: string;
+  date: string;
+  description: string;
+  banner: string;
+  bannerAlt: string;
+};
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export async function getAllPostSlugs() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        slug: fileName.replace(/\.mdx$/, ''),
+      },
+    };
+  });
+}
+
+export async function getBlogPostData() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    const slug = fileName.replace(/\.mdx$/, '');
+
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    const matterResult = matter(fileContents);
+
+    return {
+      slug,
+      ...(matterResult.data as Frontmatter),
+    };
+  });
+
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+export async function getPostData(slug: string) {
+  const fullPath = path.join(postsDirectory, `${slug}.mdx`);
+  const mdxSource = fs.readFileSync(fullPath, 'utf8');
+
+  const { code, frontmatter } = await bundleMDX({
+    source: mdxSource,
+    mdxOptions(options) {
+      options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm];
+
+      return options;
+    },
+  });
+
+  return {
+    slug,
+    frontmatter,
+    code,
+  };
+}
