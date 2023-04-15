53c52221dab9c542c9f4a5eb2503760766af9255
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-04-15 08:00:58 +0530

    Add syntax higlighting for code blocks.

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,12 +2,61 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import { bundleMDX } from 'mdx-bundler';
+
 import remarkGfm from 'remark-gfm';
-import { Frontmatter } from '@/interfaces/blog';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings/lib';
+import { remarkCodeBlocksShiki } from '@kentcdodds/md-temp';
+
+import { Frontmatter } from '@/interfaces/blog';
+import type * as H from 'hast';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
+
+function trimCodeBlocks() {
+  return async function transformer(tree: H.Root) {
+    const { visit } = await import('unist-util-visit');
+    visit(tree, 'element', (preNode: H.Element) => {
+      if (preNode.tagName !== 'pre' || !preNode.children.length) {
+        return;
+      }
+      const codeNode = preNode.children[0];
+      if (
+        !codeNode ||
+        codeNode.type !== 'element' ||
+        codeNode.tagName !== 'code'
+      ) {
+        return;
+      }
+      const [codeStringNode] = codeNode.children;
+      if (!codeStringNode) return;
+
+      if (codeStringNode.type !== 'text') {
+        console.warn(
+          `trimCodeBlocks: Unexpected: codeStringNode type is not "text": ${codeStringNode.type}`
+        );
+        return;
+      }
+      codeStringNode.value = codeStringNode.value.trim();
+    });
+  };
+}
+
+function removePreContainerDivs() {
+  return async function preContainerDivsTransformer(tree: H.Root) {
+    const { visit } = await import('unist-util-visit');
+    visit(
+      tree,
+      { type: 'element', tagName: 'pre' },
+      function visitor(node, index, parent) {
+        if (parent?.type !== 'element') return;
+        if (parent.tagName !== 'div') return;
+        if (parent.children.length !== 1 && index === 0) return;
+        Object.assign(parent, node);
+      }
+    );
+  };
+}
 
 export async function getAllPostSlugs() {
   const fileNames = fs.readdirSync(postsDirectory);
@@ -51,6 +100,9 @@ export async function getPostData(slug: string) {
         ...(options.rehypePlugins ?? []),
         rehypeSlug,
         [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+        trimCodeBlocks,
+        remarkCodeBlocksShiki,
+        removePreContainerDivs,
       ];
 
       return options;
